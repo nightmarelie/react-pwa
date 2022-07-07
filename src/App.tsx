dd015e144f3d193187eb524f19bf8505abf1f8b6
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-06 21:09:33 -0600

    feat: add app file

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { fetchWeather } from "./api";
+
+function App() {
+  return <div className="app">1</div>;
+}
+
+export default App;
---
+++ src/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = "f33a484cf794d08d0148764789aaba32";
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+
+export const fetchWeather = async (query: string) => {
+  const { data } = axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+
+  return data;
+};
