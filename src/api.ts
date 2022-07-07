436516f17a519de636c014761c645bf7027bf982
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-06 21:20:10 -0600

    feat: add layout for the site

--- src/api.ts
+++ src/api.ts
@@ -3,8 +3,22 @@ import axios from "axios";
 const API_KEY = "f33a484cf794d08d0148764789aaba32";
 const URL = "https://api.openweathermap.org/data/2.5/weather";
 
+type Weather = {
+  icon: string;
+  description: string;
+};
+
+export type Data = {
+  name: string;
+  sys: { country: string };
+  main: {
+    temp: number;
+  };
+  weather: Weather[];
+};
+
 export const fetchWeather = async (query: string) => {
-  const { data } = await axios.get<any>(URL, {
+  const { data } = await axios.get<Data>(URL, {
     params: {
       q: query,
       units: "metric",
